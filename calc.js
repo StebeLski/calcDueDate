b400b4a3f00710b3d2d6f5e5274f7787318a75b0
stebeLski <antonstebelski@gmail.com>
2020-03-13 00:58:22 +0200

    move all helpers function to utils folder

--- calc.js
+++ calc.js
@@ -1,54 +1,4 @@
-function validateSubmitDate(value) {
-  // TODO check if submit date have valid format
-  // TODO split this validator to error handling and requirements validation
-  // if (!(value instanceof Date)) {
-  //   throw new Error('Submit date must be a Date type')
-  // }
-
-  const startDate = Date.parse(value);
-
-  if (isNaN(startDate)) {
-    throw new Error('Please, provide valid data format');
-  }
-
-  const dateRightFormat = new Date(value);
-  const checkDay = dateRightFormat.getDay();
-  const checkHour = dateRightFormat.getHours();
-
-  if (checkDay === 6 || checkDay === 0) {
-    throw new Error('Please, provide working day (Mon to Fri)');
-  }
-  if (checkHour < 8 || checkHour > 16) {
-    throw new Error('Please, provide working hours (from 9AM to 17PM)');
-  }
-
-  return true;
-}
-
-function validateTurnAround(value) {
-  if (!(value === parseInt(value, 10))) {
-    throw new Error('Please, provide integer number of turnaround hours');
-  }
-
-  return true;
-}
-
-function validateDecorator(f, checks) {
-  return function() {
-    checks[0](arguments[0]);
-    checks[1](arguments[1]);
-
-    return f.apply(this, arguments);
-  };
-}
-
-function isWeekends(date) {
-  const day = date.getDay();
-  if (day === 6) {
-    return true;
-  }
-  return false;
-}
+const { validateSubmitDate, validateTurnAround, validateDecorator, isWeekends } = require('./utils/helpers');
 
 function CalculateDueDate(submitDate, turnaround) {
   addingDays = () => {
@@ -62,8 +12,10 @@ function CalculateDueDate(submitDate, turnaround) {
   const restHours = turnaround % 8;
   let startDate = new Date(submitDate);
   let dateHours = startDate.getHours();
+
   startDate.setHours(9);
   dateHours += restHours - 9;
+
   for (let i = 0; i < fullDays; i++) {
     addingDays();
   }
